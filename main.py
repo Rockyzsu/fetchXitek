867d5c57a10b62fe27034b6bd32c63629dc8ecc9
rocky v3k <jinweizsu@139.com>
2016-07-09 18:51:48 +0800

    完成全部功能，剩下的修复bug

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #-*-coding=utf-8-*-
 __author__ = 'rocchen'
 from bs4 import BeautifulSoup
-import urllib2,sys,StringIO,gzip,time,random,re
+import urllib2,sys,StringIO,gzip,time,random,re,urllib,os
 reload(sys)
 sys.setdefaultencoding('utf-8')
 class Xitek():
@@ -11,43 +11,60 @@ class Xitek():
         self.headers={"User-Agent":user_agent}
         self.last_page=self.__get_last_page()
 
-    def __getContent(self,url):
-        req=urllib2.Request(url,headers=self.headers)
-        resp=urllib2.urlopen(req)
-        content=resp.read()
-        #print content
-        data=StringIO.StringIO(content)
-        gzipper = gzip.GzipFile(fileobj=data)
-        html=gzipper.read()
-        print html
-        return html
-
-    def __getContentNoZip(self,url):
-        req=urllib2.Request(url,headers=self.headers)
-        resp=urllib2.urlopen(req)
-        time.sleep(2*random.random())
-        content=resp.read()
-
-        #print content
-        #data=StringIO.StringIO(content)
-        #gzipper = gzip.GzipFile(fileobj=data)
-        #html=gzipper.read()
-        print content
-        return content
-
 
     def __get_last_page(self):
-        html=self.__getContent(self.url)
+        html=self.__getContentAuto(self.url)
         bs=BeautifulSoup(html,"html.parser")
         page=bs.find_all('a',class_="blast")
         last_page=page[0]['href'].split('/')[-1]
-        return  int(last_page)
+        return int(last_page)
+
+
+    def __getContentAuto(self,url):
+        req=urllib2.Request(url,headers=self.headers)
+        resp=urllib2.urlopen(req)
+        #time.sleep(2*random.random())
+        content=resp.read()
+        info=resp.info().get("Content-Encoding")
+        if info==None:
+            return content
+        else:
+            t=StringIO.StringIO(content)
+            gziper=gzip.GzipFile(fileobj=t)
+            html = gziper.read()
+            return html
+
+    #def __getFileName(self,stream):
+
 
     def __download(self,url):
-        p=re.compile(r'photoid')
-        html=self.__getContent(url)
+        p=re.compile(r'href="(/photoid/\d+)"')
+        #html=self.__getContentNoZip(url)
+
+        html=self.__getContentAuto(url)
+
+        content = p.findall(html)
+        for i in content:
+            print i
+
+            photoid=self.__getContentAuto(self.url+i)
+            bs=BeautifulSoup(photoid,"html.parser")
+            final_link=bs.find('img',class_="mimg")['src']
+            print final_link
+            #pic_stream=self.__getContentAuto(final_link)
+            title=bs.title.string.strip()
+            filename = re.sub('[\/:*?"<>|]', '-', title)
+            filename=filename+'.jpg'
+            urllib.urlretrieve(final_link,filename)
+            #f=open(filename,'w')
+            #f.write(pic_stream)
+            #f.close()
         #print html
-        bs=BeautifulSoup(html,"html.parser")
+        #bs=BeautifulSoup(html,"html.parser")
+        #content=bs.find_all(p)
+        #for i in content:
+        #    print i
+        '''
         print bs.title
         element_link=bs.find_all('div',class_="element")
         print len(element_link)
@@ -56,25 +73,28 @@ class Xitek():
 
             #print type(href)
             #print href.tag
-            '''
+        '''
+        '''
             if href.children[0]:
                 print href.children[0]
-            '''
+        '''
+        '''
             t=0
 
             for i in href.children:
                 #if i.a:
                 if t==0:
                     #print k
-                    link=i['href']
+                    if i['href']
                     print link
-                    if p.findall(link):
-                        full_path=self.url[0:len(self.url)-1]+link
-                        sub_html=self.__getContent(full_path)
-                        bs=BeautifulSoup(sub_html,"html.parser")
-                        final_link=bs.find('img',class_="mimg")['src']
-                        #time.sleep(2*random.random())
-                        print final_link
+
+                        if p.findall(link):
+                            full_path=self.url[0:len(self.url)-1]+link
+                            sub_html=self.__getContent(full_path)
+                            bs=BeautifulSoup(sub_html,"html.parser")
+                            final_link=bs.find('img',class_="mimg")['src']
+                            #time.sleep(2*random.random())
+                            print final_link
                     #k=k+1
                 #print type(i)
                 #print i.tag
@@ -84,17 +104,19 @@ class Xitek():
                 t=t+1
                 #print "*"
 
-            '''
+        '''
+
+        '''
             if href:
                 if href.children:
                     print href.children[0]
-            '''
+        '''
             #print "one element link"
 
 
 
     def getPhoto(self):
-        '''
+
         start=0
         #use style/0
         photo_url="http://photo.xitek.com/style/0/p/"
@@ -103,18 +125,22 @@ class Xitek():
             print url
             #time.sleep(1)
             self.__download(url)
-        '''
 
-        url="http://photo.xitek.com/style/0/p/0"
+        '''
+        url="http://photo.xitek.com/style/0/p/10"
         self.__download(url)
         '''
-        url="http://photo.xitek.com/style/0/p/0"
+        #url="http://photo.xitek.com/style/0/p/0"
         #html=self.__getContent(url)
         #url="http://photo.xitek.com/"
-        html=self.__getContentNoZip(url)
-        print html
-        '''
+        #html=self.__getContentNoZip(url)
+        #print html
+        #'''
 def main():
+    sub_folder = os.path.join(os.getcwd(), "content")
+    if not os.path.exists(sub_folder):
+        os.mkdir(sub_folder)
+    os.chdir(sub_folder)
     obj=Xitek()
     obj.getPhoto()
 
